1f18c2409eddbc86f00ab566d97a0aba861d612d
nevets615 <surj316@yahoo.com>
2019-05-13 16:51:29 -0400

    working on my testing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,22 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe("enhancer.js", () => {
+    discribe("succeed() method", () => {
+        it(" accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success")
+    })
+})
+
+
+
+
+describe("enhancer.js", () => {
+    discribe("repair() method", () => {
+        it("accepts an item object and returns a new item with the durability restored to 100")
+    })
+})
+
+describe("enhancer.js", () => {
+    discribe("fail() method", () => {
+        it("accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.")
+    })
+})
