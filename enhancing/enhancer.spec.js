836681fb0c35f2981a8b280306e6211a6dfd791c
nevets615 <surj316@yahoo.com>
2019-05-13 17:11:05 -0400

    getting somewhere on succeed

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,22 +1,27 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
 describe("enhancer.js", () => {
-    discribe("succeed() method", () => {
-        it(" accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success")
-    })
-})
-
-
-
-
-describe("enhancer.js", () => {
-    discribe("repair() method", () => {
-        it("accepts an item object and returns a new item with the durability restored to 100")
-    })
-})
+  discribe("succeed() method", () => {
+    it(" accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success", () => {
+      expect().toBe();
+    });
+  });
+});
 
 describe("enhancer.js", () => {
-    discribe("fail() method", () => {
-        it("accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.")
-    })
-})
+  discribe("repair() method", () => {
+    it("accepts an item object and returns a new item with the durability restored to 100", () => {
+      const item = num;
+      const newItem = 100;
+      expect(item).toBe(newItem);
+    });
+  });
+});
+
+describe("enhancer.js", () => {
+  discribe("fail() method", () => {
+    it("accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.", () => {
+      expect().toBe();
+    });
+  });
+});
